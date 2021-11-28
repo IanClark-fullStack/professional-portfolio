847b64982ac3d41d5962ad14f80477c74f3d181c
Ian Clark <IanClark2021@u.northwestern.edu>
2021-11-27 20:39:08 -0600

    Setup Tailwind CSS as project dependency, changed npm script from react to craco per tailwind documentation and modified index.css css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [], // Tree shake unused styles by replacing with  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
