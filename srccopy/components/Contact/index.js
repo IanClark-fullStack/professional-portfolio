3e1c5f9d35503eb18746e70af3ac01e680817e88
Ian Clark <IanClark2021@u.northwestern.edu>
2021-12-27 12:29:04 -0600

    about

--- srccopy/components/Contact/index.js
+++ srccopy/components/Contact/index.js
@@ -2,25 +2,7 @@ import ContactForm from './ContactForm';
 // import { useState } from 'react';
 
 export default function Contact() {
-    // const externalLinks = [{
-    //         name: 'linkedIn', 
-    //         url: 'https://www.linkedin.com/in/ianclark-fullstack/',
-    // },
 
-    // {    
-    //     name: 'gitHub',
-    //     url: 'https://github.com/IanClark-fullStack',
-    // },
-    // {
-    //     name: 'are.na',
-    //     url: 'https://www.are.na/ian-clark-h9y5gqm3qei'
-    // }];
-    // // styles = {
-    // //     hoverOver = {
-    // //         borderBottom: '2px', 
-
-    // //     }
-    // // }
     
         
 
